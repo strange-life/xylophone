f5dcfd228425ff73e38cdcb5dc56fec064b2844c
strange-life <strange.hao@gmail.com>
2020-09-26 17:19:01 +0800

    build xylophone

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,52 @@ void main() {
 }
 
 class Xylophone extends StatelessWidget {
-  static var audioplayer = AudioCache();
+  static const keyboards = ['1', '2', '3', '4', '5', '6', '7'];
+  static const colorMap = {
+    '1': Colors.red,
+    '2': Colors.orange,
+    '3': Colors.yellow,
+    '4': Colors.green,
+    '5': Colors.teal,
+    '6': Colors.blue,
+    '7': Colors.purple,
+  };
+  static final audioplayer = AudioCache();
+
+  Xylophone() {
+    audioplayer.loadAll([
+      'note1.wav',
+      'note2.wav',
+      'note3.wav',
+      'note4.wav',
+      'note5.wav',
+      'note6.wav',
+      'note7.wav',
+    ]);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Xylophone',
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                audioplayer.play('note1.wav');
-              },
-              child: Text('play'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: keyboards.fold([], (acc, keyboard) {
+              acc.add(Expanded(
+                child: FlatButton(
+                  child: null,
+                  color: colorMap[keyboard],
+                  onPressed: () {
+                    audioplayer.play('note$keyboard.wav');
+                  },
+                ),
+              ));
+
+              return acc;
+            }),
           ),
         ),
       ),
