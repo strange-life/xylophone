402bbad551d8199e02f2828effcda612f8311a8c
strange-life <strange.hao@gmail.com>
2020-09-26 15:33:57 +0800

    play audio

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() {
   runApp(Xylophone());
 }
 
 class Xylophone extends StatelessWidget {
+  static var audioplayer = AudioCache();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Xylophone',
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: FlatButton(
+              onPressed: () {
+                audioplayer.play('note1.wav');
+              },
+              child: Text('play'),
+            ),
+          ),
         ),
       ),
     );
